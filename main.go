a1bc41a9fb6fcf7c8c815a9e899b93ac01569ee6
FabioSebs <sebsphotosofficial@gmail.com>
2021-10-14 02:01:10 -0400

    go-echarts updated

--- main.go
+++ main.go
@@ -8,14 +8,23 @@ import (
 	"github.com/go-echarts/go-echarts/v2/types"
 )
 
-func generateLineItems(algo func(int) []int) []opts.LineData {
+func generateLineItems(algo func(int) []int, n int) []opts.LineData {
 	items := make([]opts.LineData, 0)
-	values := algo(10)
+	values := algo(n)
 
 	for _, v := range values {
 		items = append(items, opts.LineData{Value: v})
 	}
 	return items
+}
+
+func getX(algo func(int) []int, n int) []int {
+	points := []int{}
+
+	for i := range algo(n) {
+		points = append(points, i)
+	}
+	return points
 }
 
 //Exponential Sequence
@@ -51,21 +60,35 @@ func example2(n int) []int {
 func httpserver(w http.ResponseWriter, _ *http.Request) {
 	//Create a new line instance
 	line := charts.NewLine()
+	line2 := charts.NewLine()
 
 	//Set some global option
 	line.SetGlobalOptions(
 		charts.WithInitializationOpts(opts.Initialization{Theme: types.ThemeWesteros}),
 		charts.WithTitleOpts(opts.Title{
 			Title:    "Asymptotic Analysis of Algorithm",
-			Subtitle: "AOA Forum",
+			Subtitle: "Example 1",
 		}))
 
-	line.SetXAxis([]string{"start", "end"}).
-		AddSeries("Example 1", generateLineItems(example1)).
-		AddSeries("Example 2", generateLineItems(example2)).
+	line2.SetGlobalOptions(
+		charts.WithInitializationOpts(opts.Initialization{Theme: types.ThemeWesteros}),
+		charts.WithTitleOpts(opts.Title{
+			Title:    "Asymptotic Analysis of Algorithm",
+			Subtitle: "Example 2",
+		}))
+
+	line.SetXAxis(getX(example1, 10)).
+		AddSeries("Example 1", generateLineItems(example1, 10)).
+		AddSeries("Example 2", generateLineItems(example2, 10)).
+		SetSeriesOptions(charts.WithLineChartOpts(opts.LineChart{Smooth: true}))
+
+	line2.SetXAxis(getX(example2, 10)).
+		AddSeries("Example 1", generateLineItems(example1, 10)).
+		AddSeries("Example 2", generateLineItems(example2, 10)).
 		SetSeriesOptions(charts.WithLineChartOpts(opts.LineChart{Smooth: true}))
 
 	line.Render(w)
+	line2.Render(w)
 }
 
 func main() {
